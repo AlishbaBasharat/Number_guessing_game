d63674c062883f2feb39dbf2e8161c454f3c4a74
Alishba Basharat <166277441+AlishbaBasharat@users.noreply.github.com>
2024-04-20 05:42:44 -0700

    creadted number guessing game

---
+++ index.ts
@@ -0,0 +1,18 @@
+#! /usr/bin/env node
+import inquirer from "inquirer"
+
+const randomNumber = Math.floor(Math.random() * 6 + 1);
+const answer = await inquirer.prompt([
+    { type: "number",
+    name: "userguessnumber",
+    message: "please guess a number between 1 to 6:",
+},
+]);
+
+if (answer.userguessnumbe === randomNumber) {
+    console.log("Great! you guess correct number");
+    
+} else {
+    console.log("oh sorry! you guess wrong number");
+    
+}
